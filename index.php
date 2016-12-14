4ae7fb9be498c8747f03cfe74562c983b56db888
Petr Besir Horacek <petr@gameeapp.com>
2016-12-14 01:30:13 +0100

    Changed links in default project index.php file

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <div style="text-align:center;">
-<strong>GitHub: </strong><a href="https://github.com/skiy-dockerfile/nginx-php7" target="_blank">https://github.com/skiy-dockerfile/nginx-php7</a><br />
-<strong>Docker Hub: </strong><a href="https://hub.docker.com/r/skiychan/nginx-php7" target="_blank">https://hub.docker.com/r/skiychan/nginx-php7</a><br />  
+<strong>GitHub: </strong><a href="https://github.com/Legerete/nginx-php7" target="_blank">https://github.com/Legerete/nginx-php7</a><br />
+<strong>Issues: </strong><a href="https://github.com/Legerete/nginx-php7/issues" target="_blank">https://github.com/Legerete/nginx-php7/issues</a><br />
+<strong>Docker Hub: </strong><a href="https://hub.docker.com/r/legerete/nginx-php7/" target="_blank">https://hub.docker.com/r/legerete/nginx-php7</a><br />
 </div>
 
 <?php
